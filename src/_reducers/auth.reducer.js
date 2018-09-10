5f6f2fd57fd4cdddfdc5144af721707f95eb6620
alon dayan <alond9990@gmail.com>
2018-09-10 14:01:15 +0300

    created registration reducer

--- src/_reducers/auth.reducer.js
+++ src/_reducers/auth.reducer.js
@@ -7,7 +7,7 @@ import { userConstants } from '../_constants';
 let user = JSON.parse(localStorage.getItem('user'));
 const initialState = user ? { loggedIn: true, user } : {};
 
-export function authentication(state = initialState, action) {
+export default function authentication(state = initialState, action) {
     switch (action.type) {
         case userConstants.LOGIN_REQUEST:
             return {
---
+++ src/_reducers/registration.reducer.js
@@ -0,0 +1,18 @@
+/**
+ * Created by alond9990 on 10/09/2018.
+ */
+
+import { userConstants } from '../_constants';
+
+export function registration(state = {}, action) {
+    switch (action.type) {
+        case userConstants.REGISTER_REQUEST:
+            return { registering: true };
+        case userConstants.REGISTER_SUCCESS:
+            return {};
+        case userConstants.REGISTER_FAILURE:
+            return {};
+        default:
+            return state
+    }
+}

925e4a0b6bacb5e23cb77ca1c49b373caa11c168
alon dayan <alond9990@gmail.com>
2018-09-10 13:42:11 +0300

    created user actions to expose public user actions

---
+++ src/_actions/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by alond9990 on 10/09/2018.
+ */
+
+import alertActions from './alert.actions';
+export { alertActions as alertActions };
+
+import userActions from './user.actions';
+export { userActions as userActions };

409d036ea8d88ec514e49b81ea15d36e56c8e747
alon dayan <alond9990@gmail.com>
2018-09-10 14:22:17 +0300

    created PrivateRoute component, that redirects you to login page if you are logged out

---
+++ src/_components/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by alond9990 on 10/09/2018.
+ */
+
+import PrivateRoute from './PrivateRoute';
+export { PrivateRoute as PrivateRoute };
+
+import HeaderNavbar from './HeaderNavbar.jsx';
+export { HeaderNavbar as HeaderNavbar };
